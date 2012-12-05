787a120f456dda8ad462b49f6acc9c1ba2c255d8
Denis Barov <dindin@dindin.ru>
2012-12-05 06:50:52 +0000

    put all modes in one string

--- sha256tgz.c
+++ sha256tgz.c
@@ -26,6 +26,8 @@ const char *
 archive_entry_pathname(struct archive_entry *);
 int64_t
 archive_entry_size(struct archive_entry *);
+char *
+SHA256_End(SHA256_CTX *context, char *buf);
 
 void
 usage(char *selfpath)
@@ -68,6 +70,16 @@ int_into_SHA256_context(SHA256_CTX *c, int i){
 		return 0;
 }
 
+void
+print_int_checksum(int in){
+		unsigned char md[SHA256_DIGEST_LENGTH];
+		SHA256_CTX c;
+		SHA256_Init(&c);
+		int_into_SHA256_context(&c, in);
+		SHA256_Final(&(md[0]),&c);
+		pt(md);
+}
+
 int
 main(int argc, char **argv)
 {
@@ -86,6 +98,9 @@ main(int argc, char **argv)
 		regex_t preg;
 		int ch;
 		int quiet;
+		char *modes[11];
+		char *modeline;
+		int modelinesize;
 
 
 		(void)realpath(argv[0], selfpath);
@@ -134,43 +149,48 @@ main(int argc, char **argv)
 				if (regexec(&preg, archive_entry_pathname(entry), 0, NULL, 0) !=0)
 				{
 						SHA256_Update(&c, archive_entry_pathname(entry), sizeof(archive_entry_pathname(entry)));
-						//SHA256_Update(&c, archive_entry_fflags_text(entry), sizeof(archive_entry_fflags_text(entry)));
-						SHA256_Init(&lc);
-						printf("%s\n",archive_entry_pathname(entry));
-						int_into_SHA256_context(&lc, archive_entry_dev(entry));
-						int_into_SHA256_context(&lc, archive_entry_mode(entry));
-						int_into_SHA256_context(&lc, archive_entry_devmajor(entry));
-						int_into_SHA256_context(&lc, archive_entry_devminor(entry));
-						int_into_SHA256_context(&lc, archive_entry_ino(entry));
-						int_into_SHA256_context(&lc, archive_entry_nlink(entry));
-						int_into_SHA256_context(&lc, archive_entry_rdevmajor(entry));
-						int_into_SHA256_context(&lc, archive_entry_rdevminor(entry));
-						int_into_SHA256_context(&lc, archive_entry_size(entry));
-
-						int_into_SHA256_context(&lc, archive_entry_uid(entry));
-						int_into_SHA256_context(&lc, archive_entry_gid(entry));
-						// calculate length of modestring
-						//mode = malloc(( archive_entry_mode(entry) == 0 ? 1 : (int)(log10(archive_entry_mode(entry))+1)));
-						//sprintf(mode, "%d", archive_entry_mode(entry));
-						//wprintf(L"%s\n", wacl);
-						//SHA256_Update(&c, mode, sizeof(acl));
-						int_into_SHA256_context(&c, archive_entry_dev(entry));
-						int_into_SHA256_context(&c, archive_entry_mode(entry));
-						int_into_SHA256_context(&c, archive_entry_devmajor(entry));
-						int_into_SHA256_context(&c, archive_entry_devminor(entry));
-						int_into_SHA256_context(&c, archive_entry_ino(entry));
-						int_into_SHA256_context(&c, archive_entry_nlink(entry));
-						int_into_SHA256_context(&c, archive_entry_rdevmajor(entry));
-						int_into_SHA256_context(&c, archive_entry_rdevminor(entry));
-						int_into_SHA256_context(&c, archive_entry_size(entry));
-
-						int_into_SHA256_context(&c, archive_entry_uid(entry));
-						int_into_SHA256_context(&c, archive_entry_gid(entry));
-						/*
-						filetype = malloc(sizeof(archive_entry_filetype(entry)));
-						snprintf(mode, sizeof(archive_entry_filetype(entry)), "%d", archive_entry_filetype(entry));
-						SHA256_Update(&c, filetype, sizeof(filetype));
-						*/
+						//printf("%s\n",archive_entry_pathname(entry));
+						modes[0] = int2charptr(archive_entry_dev(entry));
+						modes[1] = int2charptr(archive_entry_mode(entry));
+						modes[2] = int2charptr(archive_entry_devmajor(entry));
+						modes[3] = int2charptr(archive_entry_devminor(entry));
+						modes[4] = int2charptr(archive_entry_ino(entry));
+						modes[5] = int2charptr(archive_entry_nlink(entry));
+						modes[6] = int2charptr(archive_entry_rdevmajor(entry));
+						modes[7] = int2charptr(archive_entry_rdevminor(entry));
+						modes[8] = int2charptr(archive_entry_size(entry));
+						modes[9] = int2charptr(archive_entry_uid(entry));
+						modes[10] = int2charptr(archive_entry_gid(entry));
+						modelinesize = 
+										sizeof(modes[0]) +
+										sizeof(modes[1]) +
+										sizeof(modes[2]) +
+										sizeof(modes[3]) +
+										sizeof(modes[4]) +
+										sizeof(modes[5]) +
+										sizeof(modes[6]) +
+										sizeof(modes[7]) +
+										sizeof(modes[8]) +
+										sizeof(modes[9]) +
+										sizeof(modes[10]);
+						modeline = malloc(modelinesize+1);
+						snprintf(modeline,
+										modelinesize,
+										"%s%s%s%s%s%s%s%s%s%s%s",
+										modes[0],
+										modes[1],
+										modes[2],
+										modes[3],
+										modes[4],
+										modes[5],
+										modes[6],
+										modes[7],
+										modes[8],
+										modes[9],
+										modes[10]
+										);
+						SHA256_Update(&c, modeline, modelinesize);
+						free(modeline);
 						if (archive_entry_filetype(entry) == AE_IFREG)
 						{
 								asize = archive_entry_size(entry);
